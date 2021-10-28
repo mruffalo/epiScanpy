844b0f83514daee24860c0b23903caa95727384d
Matt Ruffalo <matt.ruffalo@gmail.com>
2021-10-28 15:04:47 -0400

    episcanpy/count_matrix/_atac_mtx.py: better path handling

--- episcanpy/count_matrix/_atac_mtx.py
+++ episcanpy/count_matrix/_atac_mtx.py
@@ -1,49 +1,50 @@
+from pathlib import Path
+from typing import List, Optional
 
+import anndata as ad
 import bamnostic as bs
 import numpy as np
-import anndata as ad
 import pandas as pd
-#from scipy.sparse  import  csc_matrix
 from scipy.sparse import csr_matrix
 
-MOUSE = ['1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', 
+MOUSE = ['1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
         '2', '3', '4', '5', '6', '7', '8', '9','X', 'Y']
 HUMAN = ['1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22',
         '2', '3', '4', '5', '6', '7', '8', '9','X', 'Y']
 
-def bld_atac_mtx(list_bam_files, loaded_feat, output_file_name=None,
-    path=None, writing_option='a', header=None, mode='rb',
+def bld_atac_mtx(bam_files: List[Path], loaded_feat, output_file_path: Optional[Path] = None,
+    writing_option='a', header=None, mode='rb',
     check_sq=True, chromosomes=HUMAN):
     """
     Build a count matrix one set of features at a time. It is specific of ATAC-seq data.
     It curently do not write down a sparse matrix. It writes down a regular count matrix
-    as a text file. 
-    
+    as a text file.
+
     Parameters
     ----------
 
-    list_bam_files: input must be a list of bam file names. One for each cell to 
+    list_bam_files: input must be a list of bam file names. One for each cell to
         build the count matrix for
 
     loaded_feat: the features for which you want to build the count matrix
-        
+
     output_file_name: name of the output file. The count matrix that will be written
-        down in the current directory. If this parameter is not specified, 
+        down in the current directory. If this parameter is not specified,
         the output count amtrix will be named 'std_output_ct_mtx.txt'
 
     path: path where to find the input file. The output file will be written down
     in your current directory, it is not affected by this parameter.
 
     writing_option: standard writing options for the output file. 'a' or 'w'
-        'a' to append to an already existing matrix. 'w' to overwrite any 
-        previously exisiting matrix. 
+        'a' to append to an already existing matrix. 'w' to overwrite any
+        previously exisiting matrix.
         default: 'a'
 
     header: if you want to write down the feature name specify this argument.
         Input must be a list.
 
     mode: bamnostic argument 'r' or 'w' for read and write 'b' and 's' for bam or sam
-        if only 'r' is specified, bamnostic will try to determine if the input is 
+        if only 'r' is specified, bamnostic will try to determine if the input is
         either a bam or sam file.
 
     check_sq: bamnostic argument. when reading, check if SQ entries are present in header
@@ -61,47 +62,41 @@ def bld_atac_mtx(list_bam_files, loaded_feat, output_file_name=None,
     matrix in a txt file
 
     """
-        
-    if output_file_name==None:
-        output_file_name='std_output_ct_mtx.txt'
 
+    if output_file_path is None:
+        output_file_path = Path('std_output_ct_mtx.txt')
 
-    if path==None:
-        path=''
-    
     # open file to write
-    output_file = open(path+output_file_name, writing_option)
-    # write header if specified
-    if header != None:
-        output_file.write('sample_name\t')
-        for feature in header:
-            output_file.write(feature)
-            output_file.write('\t')
-        output_file.write('\n')
-    # close file to write   
-    output_file.close()
+    with open(output_file_path, writing_option) as output_file:
+        # write header if specified
+        if header is not None:
+            output_file.write('sample_name\t')
+            for feature in header:
+                output_file.write(feature)
+                output_file.write('\t')
+            output_file.write('\n')
 
     # start going through the bam files
-    for name_file in list_bam_files[0:]:
+    for bam_file in bam_files:
 
         ## important variables for output
-        index_feat = {key: 0 for key in chromosomes}    
+        index_feat = {key: 0 for key in chromosomes}
         val_feat = {key: [0 for x in range(len(loaded_feat[key]))] for key in chromosomes}
-    
+
         ## PART 1 read the bam file
         keep_lines = []
         #samfile = bs.AlignmentFile(path+output_file_name, mode="rb", check_sq=False)
-        samfile = bs.AlignmentFile(path+name_file, mode="rb", check_sq=False)
+        samfile = bs.AlignmentFile(bam_file, mode="rb", check_sq=False)
         #for read in samfile.fetch(until_eof=True):
         for read in samfile:
             line = str(read).split('\t')
             if line[2][3:] in chromosomes:
                 keep_lines.append(line[2:4])
             ### print -- output
-        print(name_file, len(keep_lines), 'mapped reads')
+        print(bam_file, len(keep_lines), 'mapped reads')
         samfile.close()
-        
-        ## PART2 reads that fall into 
+
+        ## PART2 reads that fall into
         for element in keep_lines:
             ## 2 things per line:
             chrom = element[0][3:]
@@ -122,11 +117,11 @@ def bld_atac_mtx(list_bam_files, loaded_feat, output_file_name=None,
                         val_feat[chrom][pointer_feat_pos] += 1
                     else:
                         index_feat[chrom] = max_value_index
-                    # Check the following features without updating the pointer. 
+                    # Check the following features without updating the pointer.
                     break
                 else:
                     break
-     
+
             for feat_pos in loaded_feat[chrom][pointer_feat_pos:]:
                 # +1 if reads fall into more than one feature
                 if feat_pos[0] < read_pos:
@@ -138,26 +133,24 @@ def bld_atac_mtx(list_bam_files, loaded_feat, output_file_name=None,
                 else:
                     print('error')
                     break
-                    
+
         # PART 3
         # open
-        output_file = open(path+output_file_name, 'a')
-        # write down the result of the cell
-        output_file.write(name_file)
-        output_file.write('\t')
-        for chrom in chromosomes:
-            output_file.write('\t'.join([str(p) for p in val_feat[chrom]]))
+        with open(output_file_path, 'a') as output_file:
+            # write down the result of the cell
+            output_file.write(bam_file.name)
             output_file.write('\t')
-        output_file.write('\n')
-        #close
-        output_file.close()
-    
+            for chrom in chromosomes:
+                output_file.write('\t'.join([str(p) for p in val_feat[chrom]]))
+                output_file.write('\t')
+            output_file.write('\n')
+
 def read_mtx_bed(file_name, path='', omic='ATAC'):
     """
     read this specific matrix format. It is the standard output of bedtools when you merge bam files.
     """
     peak_name = []
-    cell_matrix = [] 
+    cell_matrix = []
     with open(path+file_name) as f:
         head = f.readline().split('\t')
         head[len(head)-1] = head[len(head)-1].split("\n")[0]
@@ -172,7 +165,7 @@ def read_mtx_bed(file_name, path='', omic='ATAC'):
     adata = ad.AnnData(cell_matrix,
                    obs=pd.DataFrame(index=cell_names),
                    var=pd.DataFrame(index=peak_name))
-    if omic != None:
+    if omic is not None:
         adata.uns['omic'] = omic
     return(adata)
 
@@ -184,16 +177,16 @@ def save_sparse_mtx(initial_matrix, output_file='.h5ad', path='', omic='ATAC', b
     Parameters
     ----------
 
-    initial_matrix: initial dense count matrix to load and convert into a sparse matrix. 
+    initial_matrix: initial dense count matrix to load and convert into a sparse matrix.
     If bed = True,  initial_matrix should be the path to the bed file.
 
     output_file: name of the output file for the AnnData object.
     Default output is the name of the input file with .h5ad extension
 
-    path: path to the input count matrix. The AnnData object is written in the current directory, 
+    path: path to the input count matrix. The AnnData object is written in the current directory,
         not the location specified in path.
 
-    omic: 'ATAC', 'RNA' or 'methylation' are the 3 currently recognised omics in epiScanpy. 
+    omic: 'ATAC', 'RNA' or 'methylation' are the 3 currently recognised omics in epiScanpy.
         However, other omic name can be accepted but are not yet recognised in other functions.
         default: 'ATAC'
 
@@ -211,7 +204,7 @@ def save_sparse_mtx(initial_matrix, output_file='.h5ad', path='', omic='ATAC', b
     cell_names = []
 
     # choice between 2 different input count matrix formats
-    if bed == True:
+    if bed:
         adata = read_mtx_bed(initial_matrix, path, omic)
     else:
         # reading the non sparse file
@@ -223,12 +216,12 @@ def save_sparse_mtx(initial_matrix, output_file='.h5ad', path='', omic='ATAC', b
                 for value in first_line:
                     if value != '':
                         new_line.append(value)
-                head =new_line.copy()
+                head = new_line.copy()
             else:
                 cell_names.append(first_line[0])
                 data = [[int(l) for l in first_line[1:-1]]]
             file = f.readlines()
-    
+
         for line in file:
             line = line[:-3].split('\t')
             cell_names.append(line[0])
@@ -237,22 +230,22 @@ def save_sparse_mtx(initial_matrix, output_file='.h5ad', path='', omic='ATAC', b
                 if value != '':
                     new_line.append(value)
             data.append([int(l) for l in new_line])
-            
 
+
+        anndata_kwargs = {}
         # convert into an AnnData object
-        if head != None:
-            adata = ad.AnnData(csr_matrix(data), obs=pd.DataFrame(index=cell_names), var=pd.DataFrame(index=head[1:]))
-        else:
-            adata = ad.AnnData(csr_matrix(data), obs=pd.DataFrame(index=cell_names))
-        
-        if omic != None:
+        if head is not None:
+            anndata_kwargs['var'] = pd.DataFrame(index=head[1:])
+        adata = ad.AnnData(csr_matrix(data), obs=pd.DataFrame(index=cell_names), **anndata_kwargs)
+
+        if omic is not None:
             adata.uns['omic'] = omic
 
     # writing the file as h5ad --> sparse matrix with minimum annotations
-    if save ==True:
+    if save:
         if output_file=='.h5ad':
             output_file = "".join([initial_matrix.split('.')[0], output_file])
-        
+
         adata.write(output_file)
 
     return(adata)
